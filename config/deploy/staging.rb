79e4b149c48120e096f15cd4e49c943159bae881
George Ornbo <george@shapeshed.com>
2010-11-05 11:24:42 +0000

    ok this is boring now. more monit cap stuff

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,15 +4,15 @@ set :branch, "master"
 
 namespace :deploy do
   task :start do
-    sudo "#{sudo} /usr/bin/monit -g #{application} start all"
+    sudo "#{sudo} /usr/sbin/monit -g #{application} start all"
   end
 
   task :stop do
-    sudo "/usr/bin/monit -g #{application} stop all"
+    sudo "/usr/sbin/monit -g #{application} stop all"
   end
 
   task :restart do
-    sudo "/usr/bin/monit -g #{application} restart all"
+    sudo "/usr/sbin/monit -g #{application} restart all"
   end
 end
 
