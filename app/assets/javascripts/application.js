94cd750cc00971ba27a656efcee5a76148d07c83
Tak Tran <contact@tutaktran.com>
2011-09-26 12:29:52 +0100

    Style trickle between slider.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -241,7 +241,8 @@ $(document).ready(function() {
   
   // jQuery UI Slider - http://www.filamentgroup.com/lab/update_jquery_ui_slider_from_a_select_element_now_with_aria_support/
   $('#settings-form select#setting_publish_from_4i, #settings-form select#setting_publish_until_4i').selectToUISlider({
-    labels: 24
+    labels: 24,
+    tooltip: false
   });
   
 });
