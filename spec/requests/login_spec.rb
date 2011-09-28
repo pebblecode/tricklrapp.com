adabe28f09c4c9b3515aaaf4d77dde9e6433d89a
Tak Tran <contact@tutaktran.com>
2011-09-28 12:23:25 +0100

    Fix login specs.

--- spec/requests/login_spec.rb
+++ spec/requests/login_spec.rb
@@ -3,28 +3,28 @@ require 'spec_helper'
 describe "Login integration tests" do
   it "should show a link to Twitter when not logged in " do
     visit root_path
-    page.html.should match /Login/
+    page.html.should match /Sign in with Twitter/
   end
 
   it "should correctly log in a user via twitter" do
     visit root_path
-    click_link('Login')
+    click_link('Sign in with Twitter')
     page.html.should match /Trickle it!/
   end
 
   it "should correctly log out a user" do
     visit root_path
-    click_link('Login')
+    click_link('Sign in with Twitter')
     page.html.should match /Trickle it!/
     click_link('Logout')
-    page.html.should match /Login/
+    page.html.should match /Sign in with Twitter/
   end
 end
 
 describe "Login specs" do
   it "should create a new user and a new authentication if the user never logged before " do
     visit root_path
-    click_link('Login')
+    click_link('Sign in with Twitter')
     Authentication.count.should eql 1
     User.count.should eql 1
   end
@@ -33,7 +33,7 @@ describe "Login specs" do
     other_user = Factory(:user, :name => "Somebodoy", :nickname => "some")
     auth = Factory(:authentication)
     visit root_path
-    click_link('Login')
+    click_link('Sign in with Twitter')
     Authentication.count.should eql 1
     User.count.should eql 2
   end
