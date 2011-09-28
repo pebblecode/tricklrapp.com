7bd373dd7b538ff540faf79005cd7c929ba5103e
Tak Tran <contact@tutaktran.com>
2011-09-28 11:41:30 +0100

    Add spec_helper code to spark prefork, and add some tips from rails cast.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,29 +9,34 @@ Spork.prefork do
   require 'shoulda/integrations/rspec2'
   require 'shoulda'
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  
+  RSpec.configure do |config|
+    config.mock_with :rspec
+    config.include Devise::TestHelpers, :type => :controller
+
+    config.use_transactional_fixtures = true
+
+    # From http://railscasts.com/episodes/285-spork
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.filter_run :focus => true
+    config.run_all_when_everything_filtered = true  
+  end
+
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.add_mock(:twitter, { 
+    :provider    => "twitter", 
+    :uid         => "36670724", 
+    :user_info   => { 
+    :name       => "George Ornbo", 
+    :nickname   => "shapeshed"
+  }, 
+    :credentials => {   
+    :token => "36670724-itwLyz641g76JitN9CTIpEw5Dtrsa7NLU7fpZ7aPXO",
+    :secret => "OcKQ907H0KUV7qzbWNGNYasdEBDsjasd5rPXtyTzi6c"
+  } 
+  })
+  
 end
 
 Spork.each_run do
 end
-
-
-RSpec.configure do |config|
-  config.mock_with :rspec
-  config.include Devise::TestHelpers, :type => :controller
-
-  config.use_transactional_fixtures = true
-end
-
-OmniAuth.config.test_mode = true
-OmniAuth.config.add_mock(:twitter, { 
-  :provider    => "twitter", 
-  :uid         => "36670724", 
-  :user_info   => { 
-  :name       => "George Ornbo", 
-  :nickname   => "shapeshed"
-}, 
-  :credentials => {   
-  :token => "36670724-itwLyz641g76JitN9CTIpEw5Dtrsa7NLU7fpZ7aPXO",
-  :secret => "OcKQ907H0KUV7qzbWNGNYasdEBDsjasd5rPXtyTzi6c"
-} 
-})
