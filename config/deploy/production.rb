989740405f3707d3cbe9c1c4834ad2dedb1d9c18
George Ornbo <george@shapeshed.com>
2011-10-03 16:15:15 +0100

    deployment updates

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -19,13 +19,19 @@ namespace :deploy do
 
   task :restart do
     sudo "/usr/sbin/monit -g tricklr restart all"
-    sudo "/etc/init.d/tricklr restart"
+    sudo "/etc/init.d/tricklr upgrade"
   end
 
   task :link_config_files do
     run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{deploy_to}/#{shared_dir}/tmp/sockets #{release_path}/tmp/sockets"
   end
+
+  desc "Precompiles assets for the Asset Pipeline"
+  task :precompile_assets do
+    run "cd #{current_path} && bundle exec rake RAILS_ENV=production RAILS_GROUPS=assets assets:precompile"
+  end
+
 end
 
 after "deploy:update_code", "deploy:link_config_files"
