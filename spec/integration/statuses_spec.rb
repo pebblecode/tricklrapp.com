e3ee9764546d41b554844bb0e9520e49b7509e83
Tak Tran <contact@tutaktran.com>
2011-10-04 15:01:39 +0100

    Remove status in text box after submitting.

--- spec/integration/statuses_spec.rb
+++ spec/integration/statuses_spec.rb
@@ -25,3 +25,29 @@ describe "A user entering erroneous statuses" do
     page.html.should match /hello, I am queued/ # Shows the Edit Status page, with no queued trickles for some reason
   end
 end
+
+describe "A user submitting a new status" do
+  before(:each) do
+    visit root_path
+    click_link('Sign in with Twitter')
+  end
+    
+  it "should see the status queued" do
+    visit root_path
+    fill_in('status_status', :with => 'hello, I am queued')
+    click_button('Trickle it!')
+    
+    within(:css, "ul#queued_statuses li") do
+      page.html.should match /hello, I am queued/
+    end
+  end
+  
+  it "should see see an empty status text box" do
+    visit root_path
+    fill_in('status_status', :with => 'hello, I am queued')
+    click_button('Trickle it!')
+
+    find_field('status[status]').value.should == ""
+  end
+    
+end
