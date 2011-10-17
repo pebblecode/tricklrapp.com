4f186a877b2633b45664d84c7a7a2dde905bb30c
Tak Tran <contact@tutaktran.com>
2011-10-17 11:40:06 +0100

    Fix up cucumber tests.
    Changed 'status' references to 'tweet'.

--- features/step_definitions/statuses_steps.rb
+++ features/step_definitions/statuses_steps.rb
@@ -1,15 +1,15 @@
-Given /^the user "([^"]*)" has queued statuses of "([^"]*)"$/ do |user, statuses|
-  statuses.split(', ').each do |status|
+Given /^the user "([^"]*)" has queued tweets of "([^"]*)"$/ do |user, tweets|
+  tweets.split(', ').each do |tweet|
     @status = Factory(:status, 
-                      :status => status, 
+                      :status => tweet, 
                       :user => User.find_by_nickname(user))
   end
 end
  
-Given /^the user "([^"]*)" has published statuses of "([^"]*)"$/ do |user, statuses|
-  statuses.split(', ').each do |status|
+Given /^the user "([^"]*)" has published tweets of "([^"]*)"$/ do |user, tweets|
+  tweets.split(', ').each do |tweet|
     @status = Factory(:status, 
-                      :status => status, 
+                      :status => tweet, 
                       :published_at => Time.now - 2.hours, 
                       :twitter_id => "1234", 
                       :user => User.find_by_nickname(user))
