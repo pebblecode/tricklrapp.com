63d32d70fefdc61a39f898a91cfef9c0491b968a
Tak Tran <contact@tutaktran.com>
2011-10-19 13:34:58 +0100

    Add pending test for publish frequency options

---
+++ spec/controllers/settings_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe SettingsController do
+
+  describe 'publish frequency options' do
+    it 'should enable setting the fo' do
+      options = ['1 mins', '5 mins', '10 mins', '20 mins', '30 mins', '1 hr', '2 hrs', '3 hrs', '4 hrs', '5 hrs', '6 hrs', '7 hrs', '8 hrs', '1 day', '2 days', '3 days', '4 days', '5 days', '6 days', '1 wk', '2 wk', '3 wk', '1 mth']
+      
+      pending('Make sure all publish frequency options are savable')
+    end
+  end
+
+end
