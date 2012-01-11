cbd632bfdefc4381971625af125941f2550fb916
George Ornbo <george@shapeshed.com>
2012-01-11 15:50:41 +0000

    adding initial error handling #25

--- app/models/send_tweet.rb
+++ app/models/send_tweet.rb
@@ -18,14 +18,49 @@ class SendTweet
 
     client = Twitter::Client.new
 
-    # We need some error handling here if Twitter is down etc...
-    response = client.update(@tweet.status)
-
-    # If we are ok log the details
-    @tweet.update_attributes(
-      :twitter_id => response.id,
-      :published_at => response.created_at
-    )
+    # NOTE for more on error codes see 
+    # https://dev.twitter.com/docs/error-codes-responses
+    begin
+      response = client.update(@tweet.status)
+      @tweet.update_attributes(
+        :twitter_id => response.id,
+        :published_at => response.created_at
+      )
+    # Raised when Twitter returns the HTTP status code 502
+    rescue Twitter::Error::BadGateway
+      @tweet.scheduled_at = Time.now.utc + 5.minutes
+      @tweet.save!
+      @tweet.requeue
+    # TODO - handle these error sceanarios
+    # Raised when Twitter returns the HTTP status code 400
+    # rescue Twitter::Error::BadRequest
+    # Raised when Twitter returns a 4xx HTTP status code
+    # rescue Twitter::Error::ClientError
+    # Raised when Twitter returns the HTTP status code 420
+    # rescue Twitter::Error::EnhanceYourCalm
+    # Raised when Twitter returns the HTTP status code 403
+    # rescue Twitter::Error::Forbidden
+    # Raised when Twitter returns the HTTP status code 500
+    # rescue Twitter::Error::InternalServerError
+    # Raised when Twitter returns the HTTP status code 406
+    # rescue Twitter::Error::NotAcceptable
+    # Raised when Twitter returns the HTTP status code 404
+    # rescue Twitter::Error::NotFound
+    # Raised when Twitter returns a 5xx HTTP status code
+    # rescue Twitter::Error::ServerError
+    # Raised when Twitter returns the HTTP status code 503
+    # rescue Twitter::Error::ServiceUnavailable
+    # Raised when Twitter returns the HTTP status code 401
+    # rescue Twitter::Error::Unauthorized
+    # rescue Errno::ENOENT
+    # Raised when there is a timeout
+    # rescue Errno::ETIMEDOUT
+    # Raised when the connection is reset 
+    # rescue Errno::ECONNRESET
+    # Raised when there is a standard error
+    # rescue
+    # Raised when there is a generic Exception
+    # rescue Exception
+    end
   end
-
 end
