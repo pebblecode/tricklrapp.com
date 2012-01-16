40dd4afcd89716afa655d15f7dec695ddaf70acb
Vincent Martínez <vincent@pebblecode.com>
2012-01-16 14:06:22 +0000

    Related to #8 - fixes javascript crash in some browsers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,12 @@
-// This is a manifest file that'll be compiled into including all the files listed below.
-// // Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
-// // be included in the compiled file accessible from http://example.com/assets/application.js
-// // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// // the compiled file.
-//
-//= require jquery.min
-//= require jquery-ui-1.8.16.custom.min
-//= require_tree .
 /*
+ * This is a manifest file that'll be compiled into including all the files listed below.
+ * * Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+ * * be included in the compiled file accessible from http://example.com/assets/application.js
+ * * It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+ * * the compiled file.
+ *= require jquery.min
+ *= require jquery-ui-1.8.16.custom.min
+ *= require_tree ./libs
  * Fades flash notices out after they are shown
 */
 jQuery.fn.flashNotice = function () {
