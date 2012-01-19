b01850146a046c534186b3b4f51ef661030ed382
George Ornbo <george@shapeshed.com>
2012-01-19 17:10:54 +0000

    amemd default times. closes #40

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120113181135) do
+ActiveRecord::Schema.define(:version => 20120119164923) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(:version => 20120113181135) do
     t.boolean  "automatic",                                   :default => false
     t.decimal  "time_digit",    :precision => 6, :scale => 1, :default => 2.0
     t.string   "time_unit",                                   :default => "hours"
-    t.datetime "publish_from"
-    t.datetime "publish_until"
+    t.time     "publish_from",                                :default => '2000-01-01 09:00:00'
+    t.time     "publish_until",                               :default => '2000-01-01 23:00:00'
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "time_zone"
@@ -49,8 +49,8 @@ ActiveRecord::Schema.define(:version => 20120113181135) do
   create_table "users", :force => true do |t|
     t.string   "nickname"
     t.string   "name"
-    t.datetime "remember_created_at"
     t.string   "remember_token"
+    t.datetime "remember_created_at"
     t.integer  "sign_in_count",       :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
