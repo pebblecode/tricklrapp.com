e540add6b76e1ec3f780209f3c435a854a081523
Tak Tran <contact@tutaktran.com>
2012-09-11 14:13:47 +0100

    Fixed up spaces.

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -6,45 +6,45 @@ describe Status do
     @status = FactoryGirl.create(:status)
   end
 
-  it "is valid with valid attributes" do 
+  it "is valid with valid attributes" do
     @status.should be_valid
   end
 
-  it "should allow a status up to 140 characters" do 
+  it "should allow a status up to 140 characters" do
     @status.status = Random.alphanumeric(140)
     @status.should be_valid
   end
-  
+
   it "should count urls as 20 characters" do
     @status.status = "http://cnn.com/#{Random.alphanumeric(150)}"
     @status.twitter_character_count.should == 20
   end
 
-  it "should not allow a status over 140 characters" do 
+  it "should not allow a status over 140 characters" do
     @status.status = Random.alphanumeric(141)
     @status.should_not be_valid
   end
 
-  it "should allow a twitter_id up to 255 characters" do 
+  it "should allow a twitter_id up to 255 characters" do
     @status.twitter_id = Random.alphanumeric(255)
     @status.should be_valid
   end
 
-  it "should not allow a twitter_id over 255 characters" do 
+  it "should not allow a twitter_id over 255 characters" do
     @status.twitter_id = Random.alphanumeric(256)
     @status.should_not be_valid
   end
 
-  it "should allow a response_code up to 255 characters" do 
+  it "should allow a response_code up to 255 characters" do
     @status.response_code = Random.alphanumeric(255)
     @status.should be_valid
   end
 
-  it "should not allow a response_code over 255 characters" do 
+  it "should not allow a response_code over 255 characters" do
     @status.response_code = Random.alphanumeric(256)
     @status.should_not be_valid
   end
-  
+
   it "should be rescheduled when publish! is called" do
     @time = Time.now.utc
     Time.stub!(:now).and_return(@time)
@@ -53,4 +53,3 @@ describe Status do
     @status.scheduled_at.should eql(Time.now.utc)
   end
 end
-
