41612c462048172cb757aa2e9eadeebdd3ad3900
Tak Tran <contact@tutaktran.com>
2012-09-12 16:17:08 +0100

    Add App.timeToString tests.

--- spec/javascripts/countdown-test.js
+++ spec/javascripts/countdown-test.js
@@ -30,4 +30,50 @@ describe('Countdown helper functions', function(){
       App.daysInTime(5 * 24 * 60 * 60 * 1000).should.equal(5);
     })
   })
+
+  describe('App.timeToString', function() {
+    describe('days', function() {
+      it('should return about 1 day', function() {
+        App.timeToString(24 * 60 * 60 * 1000).should.equal('about 1 day')
+      })
+
+      it('should return about 15 days', function() {
+        App.timeToString(15 * 24 * 60 * 60 * 1000).should.equal('about 15 days')
+      })
+    })
+
+    describe('hours', function() {
+      it('should return about 1 hour', function() {
+        App.timeToString(60 * 60 * 1000).should.equal('about 1 hour')
+      })
+
+      it('should return about 15 hours', function() {
+        App.timeToString(15 * 60 * 60 * 1000).should.equal('about 15 hours')
+      })
+    })
+
+    describe('minutes', function() {
+      it('should return about 1 minute', function() {
+        App.timeToString(60 * 1000).should.equal('about 1 minute')
+      })
+
+      it('should return about 15 minutes', function() {
+        App.timeToString(15 * 60 * 1000).should.equal('about 15 minutes')
+      })
+    })
+
+    describe('seconds', function() {
+      it('should return 0 seconds', function() {
+        App.timeToString(0).should.equal('0 seconds')
+      })
+
+      it('should return 1 second', function() {
+        App.timeToString(1000).should.equal('1 second')
+      })
+
+      it('should return 15 seconds', function() {
+        App.timeToString(15 * 1000).should.equal('15 seconds')
+      })
+    })
+  })
 })
